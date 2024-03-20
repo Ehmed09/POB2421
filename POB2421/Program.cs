4a0663424448c91af2b76e84c53b01374169240b
Ehmed09 <askerov.ehmed@mail.ru>
2024-03-20 20:38:44 +0200

    second

--- POB2421/Program.cs
+++ POB2421/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//float d = 450f;
+//int i = (int)d;
+////byte b = (byte)i;
+////Console.WriteLine(b);
+//string myName = "Ахмед\n" +
+//    "" + "Аскеров";
+//Console.WriteLine(myName);
+//int myString = 284;
+// int b =  int.Parse("myString");
+//Console.WriteLine(b);
+using System.Diagnostics;
+
+byte bt = byte.Parse(Console.ReadLine());
+long freq = Stopwatch.Frequency;
+Stopwatch stopwatch = new Stopwatch();
+stopwatch.Start();
+int integer = (int)bt;
+stopwatch.Stop();
+decimal sec = (decimal)stopwatch.ElapsedTicks / freq;
+Console.WriteLine ($"bt = {bt}\r\n Частота таймера "+$" {freq} такс/с \r \n Время в тактах"+$" {stopwatch.ElapsedTicks} \r \n " +
+    $"Время в секундах "+$" {sec}");
+Console.WriteLine(integer);
