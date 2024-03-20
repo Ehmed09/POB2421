d5248b748cb73063ed85f950de1010349d496d22
Ehmed09 <askerov.ehmed@mail.ru>
2024-03-20 21:06:07 +0200

    2

--- POB2421/Program.cs
+++ POB2421/Program.cs
@@ -7,18 +7,26 @@
 //string myName = "Ахмед\n" +
 //    "" + "Аскеров";
 //Console.WriteLine(myName);
-//int myString = 284;
-// int b =  int.Parse("myString");
-//Console.WriteLine(b);
-using System.Diagnostics;
+////int myString = 284;
+//// int b =  int.Parse("myString");
+////Console.WriteLine(b);
+//using System.Diagnostics;
 
-byte bt = byte.Parse(Console.ReadLine());
-long freq = Stopwatch.Frequency;
-Stopwatch stopwatch = new Stopwatch();
-stopwatch.Start();
-int integer = (int)bt;
-stopwatch.Stop();
-decimal sec = (decimal)stopwatch.ElapsedTicks / freq;
-Console.WriteLine ($"bt = {bt}\r\n Частота таймера "+$" {freq} такс/с \r \n Время в тактах"+$" {stopwatch.ElapsedTicks} \r \n " +
-    $"Время в секундах "+$" {sec}");
-Console.WriteLine(integer);
+//byte bt = byte.Parse(Console.ReadLine());
+//long freq = Stopwatch.Frequency;
+//Stopwatch stopwatch = new Stopwatch();
+
+//stopwatch.Start();
+//int integer = (int)bt;
+//stopwatch.Stop();
+//decimal sec = (decimal)stopwatch.ElapsedTicks / freq;
+//Console.WriteLine ($"bt = {bt}\r\n Частота таймера "+$" {freq} такс/с \r \n Время в тактах"+$" {stopwatch.ElapsedTicks} \r \n " +
+//    $"Время в секундах "+$" {sec}");
+//Console.WriteLine(integer);
+
+//Console.WriteLine($"{Math.PI:F2}");
+//Console.WriteLine($"{Math.E:F1}");
+
+Console.WriteLine("2 кг\n13 17");
+
+
